cf8932fb05916abf670cab2492d63cdf617bec98
osalomaoleme <salomao.leme96@gmail.com>
2025-02-20 21:21:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,21 @@
 const CACHE_NAME = "controle-validade-v1";
 const urlsToCache = [
-  "./",               // Caminho relativo para raiz
-  "./index.html",     
-  "./manifest.json",  
-  // Se você tiver um style.css e script.js separados, adicione-os aqui:
-  // "./style.css",
-  // "./script.js",
-  "./icons/icone.png" // Ajuste se você tiver mais ícones
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icone.512.png"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
